28627aecbfedaee491c07bade6accfe2c1e675b2
diegomansilla <diiego.mansilla@gmail.com>
2024-02-07 10:35:27 -0300

    Seguridad en la BD

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //Probando store procedure
+        DB::unprepared('CALL borrar_accesos();');
     }
 }
